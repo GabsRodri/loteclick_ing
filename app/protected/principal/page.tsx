1ac6a91cfac8b09056451444c128f597a6d1e7cf
Gaby <gabypro2015@gmail.com>
2025-05-20 06:11:04 -0500

    Update page.tsx

--- app/protected/principal/page.tsx
+++ app/protected/principal/page.tsx
@@ -54,6 +54,7 @@ return (
       </p>
     </div>
 
+    {/* Cards */}
     <div className="mt-10 grid gap-6 sm:gap-8 md:gap-10 lg:gap-14 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center">
       {role === "admin" && (
         <>
