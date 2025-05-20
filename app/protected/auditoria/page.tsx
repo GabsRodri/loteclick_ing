d56b6506eefce92d601552977fb063975b3bd6db
Gaby <gabypro2015@gmail.com>
2025-05-20 05:55:51 -0500

    Update page.tsx

--- app/protected/auditoria/page.tsx
+++ app/protected/auditoria/page.tsx
@@ -209,143 +209,143 @@ const handleRestore = async (auditoria: Auditoria) => {
   const totalPages = Math.ceil(filteredAuditoriaData.length / itemsPerPage);
 
   return (
-    <main style={{ padding: "20px", minHeight: "100vh", width: "100%", boxSizing: "border-box" }}>
-      {/* Notificación */}
-      {mensaje && (
-        <div
-          className={`mb-6 col-span-full p-3 text-sm rounded-md text-background ${mensaje.tipo === "success" ? "bg-green-600 dark:bg-green-950" : "bg-red-600 dark:bg-red-950"
-            }`}
-        >
-          {mensaje.texto}
+  <main style={{ padding: "20px", minHeight: "100vh", width: "100%", boxSizing: "border-box" }}>
+    {/* Notificación */}
+    {mensaje && (
+      <div
+        className={`mb-6 col-span-full p-3 text-sm rounded-md text-background ${mensaje.tipo === "success" ? "bg-green-600 dark:bg-green-950" : "bg-red-600 dark:bg-red-950"
+          }`}
+      >
+        {mensaje.texto}
+      </div>
+    )}
+
+    {/* Contenedor principal */}
+    <div className="p-4 sm:p-6 md:p-8 border rounded-lg shadow-md bg-background dark:border-gray-700">
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-xl font-semibold text-primary dark:text-gray-100">Historial de Cambios</h2>
+      </div>
+
+      {/* Filtros */}
+      <div className="border p-3 rounded-md grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 bg-background shadow-sm dark:border-gray-700">
+        <div>
+          <label className="block mb-1 font-medium text-sm dark:text-gray-100">Usuario</label>
+          <input
+            type="text"
+            placeholder="Filtrar por usuario"
+            value={filters.usuario}
+            onChange={(e) => setFilters({ ...filters, usuario: e.target.value })}
+            className="w-full border p-1 rounded-md bg-background shadow-sm focus:outline-none focus:ring-2 focus:ring-primary dark:border-gray-700 dark:focus:ring-zinc-800"
+          />
         </div>
-      )}
-      {/* Contenedor principal */}
-      <div className="p-6 border rounded-lg shadow-md bg-background dark:border-gray-700">
-        <div className="flex justify-between items-center mb-4">
-          <h2 className="text-xl font-semibold text-primary dark:text-gray-100">Historial de Cambios</h2>
+        <div>
+          <label className="block mb-1 font-medium text-sm dark:text-gray-100">Operación</label>
+          <input
+            type="text"
+            placeholder="Filtrar por operación"
+            value={filters.operacion}
+            onChange={(e) => setFilters({ ...filters, operacion: e.target.value })}
+            className="w-full border p-1 rounded-md bg-background shadow-sm focus:outline-none focus:ring-2 focus:ring-primary dark:border-gray-700 dark:focus:ring-zinc-800"
+          />
         </div>
-
-        {/* Filtros */}
-        <div className="border p-3 rounded-md grid sm:grid-cols-2 lg:grid-cols-4 gap-3 bg-background shadow-sm dark:border-gray-700">
-          <div>
-            <label className="block mb-1 font-medium text-sm dark:text-gray-100">Usuario</label>
-            <input
-              type="text"
-              placeholder="Filtrar por usuario"
-              value={filters.usuario}
-              onChange={(e) => setFilters({ ...filters, usuario: e.target.value })}
-              className="w-full border p-1 rounded-md bg-background shadow-sm focus:outline-none focus:ring-2 focus:ring-primary dark:border-gray-700 dark:focus:ring-zinc-800"
-            />
-          </div>
-          <div>
-            <label className="block mb-1 font-medium text-sm dark:text-gray-100">Operación</label>
-            <input
-              type="text"
-              placeholder="Filtrar por operación"
-              value={filters.operacion}
-              onChange={(e) => setFilters({ ...filters, operacion: e.target.value })}
-              className="w-full border p-1 rounded-md bg-background shadow-sm focus:outline-none focus:ring-2 focus:ring-primary dark:border-gray-700 dark:focus:ring-zinc-800"
-            />
-          </div>
-          <div>
-            <label className="block mb-1 font-medium text-sm dark:text-gray-100">Tabla</label>
-            <input
-              type="text"
-              placeholder="Filtrar por tabla"
-              value={filters.tabla}
-              onChange={(e) => setFilters({ ...filters, tabla: e.target.value })}
-              className="w-full border p-1 rounded-md bg-background shadow-sm focus:outline-none focus:ring-2 focus:ring-primary dark:border-gray-700 dark:focus:ring-zinc-800"
-            />
-          </div>
-          <div>
-            <label className="block mb-1 font-medium text-sm dark:text-gray-100">Fecha</label>
-            <input
-              type="date"
-              value={filters.fecha}
-              onChange={(e) => setFilters({ ...filters, fecha: e.target.value })}
-              className="w-full border p-1 rounded-md bg-background shadow-sm focus:outline-none focus:ring-2 focus:ring-primary dark:border-gray-700 dark:focus:ring-zinc-800"
-            />
-          </div>
+        <div>
+          <label className="block mb-1 font-medium text-sm dark:text-gray-100">Tabla</label>
+          <input
+            type="text"
+            placeholder="Filtrar por tabla"
+            value={filters.tabla}
+            onChange={(e) => setFilters({ ...filters, tabla: e.target.value })}
+            className="w-full border p-1 rounded-md bg-background shadow-sm focus:outline-none focus:ring-2 focus:ring-primary dark:border-gray-700 dark:focus:ring-zinc-800"
+          />
         </div>
-
-        {/* Mostrar los datos */}
-        {loading ? (
-          <p className="text-left mt-4 text-gray-500 dark:text-gray-400">Cargando datos...</p>
-        ) : filteredAuditoriaData.length > 0 ? (
-          <div className="overflow-x-auto mt-4">
-            <table className="scrollable w-full border text-sm bg-background shadow-lg rounded-md dark:border-gray-700">
-              <thead className="bg-primary text-background dark:bg-black dark:text-gray-100">
-                <tr>
-                  <th className="border px-2 py-2 dark:border-gray-700 border-gray-300">Usuario</th>
-                  <th className="border px-2 py-2 dark:border-gray-700 border-gray-300">Operación</th>
-                  <th className="border px-2 py-2 dark:border-gray-700 border-gray-300">Tabla</th>
-                  <th className="border px-2 py-2 dark:border-gray-700 border-gray-300">Datos Antes</th>
-                  <th className="border px-2 py-2 dark:border-gray-700 border-gray-300">Datos Después</th>
-                  <th className="border px-2 py-2 dark:border-gray-700 border-gray-300">Fecha</th>
-                </tr>
-              </thead>
-              <tbody>
-                {paginateData().map((auditoria) => (
-                  <tr key={auditoria.id}>
-                    <td className="border px-1 py-1 border-gray-300 dark:border-gray-700">
-                      {auditoria.usuario_email}
-                      {auditoria.operacion === "DELETE" && (
-                        <button
-                          onClick={() => handleRestore(auditoria)}
-                          className="text-sm text-primary dark:text-gray-400 hover:underline"
-                        >
-                          Restaurar
-                        </button>
-                      )}
-                    </td>
-
-
-                    <td className="border px-1 py-1 border-gray-300 dark:border-gray-700">
-                      {auditoria.operacion}
-                    </td>
-                    <td className="border px-1 py-1 border-gray-300 dark:border-gray-700">
-                      {auditoria.tabla}
-                    </td>
-                    <td className="border px-1 py-1 border-gray-300 dark:border-gray-700 align-top max-h-[60px] overflow-y-auto">
-                      <div className="scrollable max-h-[60px] overflow-y-auto text-sm text-gray-800 dark:text-gray-100">
-                        {formatJson(auditoria.datos_antes)}
-                      </div>
-                    </td>
-                    <td className="border px-1 py-1 border-gray-300 dark:border-gray-700 align-top max-h-[60px] overflow-y-auto">
-                      <div className="scrollable max-h-[60px] overflow-y-auto text-sm text-gray-800 dark:text-gray-100">
-                        {formatJson(auditoria.datos_despues)}
-                      </div>
-                    </td>
-                    <td className="border px-1 py-1 border-gray-300 dark:border-gray-700">
-                      {formatDate(auditoria.fecha)}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        ) : (
-          <p>No se encontraron registros de auditoría.</p>
-        )}
-
-        {/* Paginación */}
-        <div className="flex justify-center mt-4">
-          <button
-            disabled={currentPage === 1}
-            onClick={() => changePage(currentPage - 1)}
-            className="bg-primary border border-primary-foreground text-background px-4 py-2 mr-8 rounded-md hover:bg-primary/90 transition-all dark:bg-black dark:text-gray-100 dark:border-gray-700 dark:hover:bg-zinc-800"
-          >
-            Anterior
-          </button>
-          <button
-            disabled={currentPage === totalPages}
-            onClick={() => changePage(currentPage + 1)}
-            className="bg-primary border border-primary-foreground text-background px-4 py-2 rounded-md hover:bg-primary/90 transition-all dark:bg-black dark:text-gray-100 dark:border-gray-700 dark:hover:bg-zinc-800"
-          >
-            Siguiente
-          </button>
+        <div>
+          <label className="block mb-1 font-medium text-sm dark:text-gray-100">Fecha</label>
+          <input
+            type="date"
+            value={filters.fecha}
+            onChange={(e) => setFilters({ ...filters, fecha: e.target.value })}
+            className="w-full border p-1 rounded-md bg-background shadow-sm focus:outline-none focus:ring-2 focus:ring-primary dark:border-gray-700 dark:focus:ring-zinc-800"
+          />
         </div>
       </div>
-    </main>
-  );
+
+      {/* Mostrar los datos */}
+      {loading ? (
+        <p className="text-left mt-4 text-gray-500 dark:text-gray-400">Cargando datos...</p>
+      ) : filteredAuditoriaData.length > 0 ? (
+        <div className="overflow-x-auto mt-4">
+          <table className="scrollable w-full border text-sm bg-background shadow-lg rounded-md dark:border-gray-700">
+            <thead className="bg-primary text-background dark:bg-black dark:text-gray-100">
+              <tr>
+                <th className="border px-2 py-2 dark:border-gray-700 border-gray-300">Usuario</th>
+                <th className="border px-2 py-2 dark:border-gray-700 border-gray-300">Operación</th>
+                <th className="border px-2 py-2 dark:border-gray-700 border-gray-300">Tabla</th>
+                <th className="border px-2 py-2 dark:border-gray-700 border-gray-300">Datos Antes</th>
+                <th className="border px-2 py-2 dark:border-gray-700 border-gray-300">Datos Después</th>
+                <th className="border px-2 py-2 dark:border-gray-700 border-gray-300">Fecha</th>
+              </tr>
+            </thead>
+            <tbody>
+              {paginateData().map((auditoria) => (
+                <tr key={auditoria.id}>
+                  <td className="border px-1 py-1 border-gray-300 dark:border-gray-700">
+                    {auditoria.usuario_email}
+                    {auditoria.operacion === "DELETE" && (
+                      <button
+                        onClick={() => handleRestore(auditoria)}
+                        className="text-sm text-primary dark:text-gray-400 hover:underline"
+                      >
+                        Restaurar
+                      </button>
+                    )}
+                  </td>
+
+                  <td className="border px-1 py-1 border-gray-300 dark:border-gray-700">
+                    {auditoria.operacion}
+                  </td>
+                  <td className="border px-1 py-1 border-gray-300 dark:border-gray-700">
+                    {auditoria.tabla}
+                  </td>
+                  <td className="border px-1 py-1 border-gray-300 dark:border-gray-700 align-top max-h-[60px] overflow-y-auto">
+                    <div className="scrollable max-h-[60px] overflow-y-auto text-sm text-gray-800 dark:text-gray-100">
+                      {formatJson(auditoria.datos_antes)}
+                    </div>
+                  </td>
+                  <td className="border px-1 py-1 border-gray-300 dark:border-gray-700 align-top max-h-[60px] overflow-y-auto">
+                    <div className="scrollable max-h-[60px] overflow-y-auto text-sm text-gray-800 dark:text-gray-100">
+                      {formatJson(auditoria.datos_despues)}
+                    </div>
+                  </td>
+                  <td className="border px-1 py-1 border-gray-300 dark:border-gray-700">
+                    {formatDate(auditoria.fecha)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <p>No se encontraron registros de auditoría.</p>
+      )}
+
+      {/* Paginación */}
+      <div className="flex justify-center mt-4">
+        <button
+          disabled={currentPage === 1}
+          onClick={() => changePage(currentPage - 1)}
+          className="bg-primary border border-primary-foreground text-background px-4 py-2 mr-8 rounded-md hover:bg-primary/90 transition-all dark:bg-black dark:text-gray-100 dark:border-gray-700 dark:hover:bg-zinc-800"
+        >
+          Anterior
+        </button>
+        <button
+          disabled={currentPage === totalPages}
+          onClick={() => changePage(currentPage + 1)}
+          className="bg-primary border border-primary-foreground text-background px-4 py-2 rounded-md hover:bg-primary/90 transition-all dark:bg-black dark:text-gray-100 dark:border-gray-700 dark:hover:bg-zinc-800"
+        >
+          Siguiente
+        </button>
+      </div>
+    </div>
+  </main>
+);
 }
